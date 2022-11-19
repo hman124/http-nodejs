462cbe2f8c7ba2c3a29138946e9044905fb266a2
Harrison Steed <oceanlevel68@gmail.com>
2022-11-19 15:51:09 -0700

    initial commit with test hello world

---
+++ server.js
@@ -0,0 +1,13 @@
+const express= require("express");
+
+const app = express();
+
+app.get("/", (req,res)=>{
+	res.send("Hello world");
+});
+
+app.get("/yes", (req,res) => {
+	res.send(Date.now());
+});
+
+app.listen(process.env.PORT || 3000);
